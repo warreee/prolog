2a29dbfc99bc48f71f9ce9ae6eedc923d55e2f2c
Ward Schodts <wardschodts@hotmail.com>
2015-08-16 01:04:55 +0200

    pfai

--- pfai/ch3_2.pl
+++ pfai/ch3_2.pl
@@ -2,3 +2,11 @@ member(X,[X|Tail]).
 
 member(X,[Head|Tail]) :-
     member(X,Tail).
+    
+member1(X, L) :-
+    conc(_,[X|_],L).
+    
+conc([],L,L).
+    
+conc([X|L1],L2,[X|L3]) :-
+    conc(L1,L2,L3).
