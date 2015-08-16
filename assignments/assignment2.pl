94d3533d8f58fa28b0d18f7c4bd337b290773cfb
Ward Schodts <wardschodts@hotmail.com>
2015-08-16 16:03:37 +0200

    Graphs

--- assignments/assignment2.pl
+++ assignments/assignment2.pl
@@ -28,5 +28,40 @@ look_up([pair(K,V)|_],K,V).
 look_up([_|T],Key,Value) :-
     look_up(T,Key,Value).
     
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Graphs%%%%%%%%%%%%%%%%%%%%%%%%%%%
+node(a).
+node(b).
+node(c).
+node(d).
+node(e).
+
+edge(a,b).
+edge(b,d).
+edge(b,c).
+edge(c,d).
+
+neighbor(A,B) :-
+    edge(A,B);
+    edge(B,A).
+    
+% indefinite loop:
+path(S,E) :- neighbor(S,E).
+path(S,E) :-
+    neighbor(S,X),
+    path(X,E).    
+
+
+path2(S,E) :- path3(S,E,[]).
+
+path3(S,E,V) :- 
+    neighbor(S,E),
+    \+ member(edge(S,E),V),
+    \+ member(edge(E,S),V).
+
+path3(S,E,V) :-
+    neighbor(S,X),
+    \+ member(edge(S,X),V),
+    \+ member(edge(X,S),V),
+    path3(X,E,[edge(S,X)|V]).
 
 
