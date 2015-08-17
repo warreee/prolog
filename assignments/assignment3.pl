8a351ff8f65ab03de06646051e1039db176b1cea
Ward Schodts <wardschodts@hotmail.com>
2015-08-17 11:32:16 +0200

    More classes

---
+++ assignments/assignment3.pl
@@ -0,0 +1,43 @@
+teaches(berbers,bs).
+teaches(berbers,iw).
+teaches(demoen,ab).
+teaches(demoen,cc).
+teaches(holvoet,bvp).
+teaches(moens,bvp).
+teaches(danny,ai).
+teaches(maurice,ai).
+teaches(dedecker,socs).
+
+takes(tom,bs).
+takes(tom,bvp).
+takes(tom,socs).
+takes(maarten,socs).
+takes(maarten,bs).
+takes(pieter,bvp).
+
+takes_same_course(X,Y) :- 
+    findall((X,Y), 
+            (takes(X,C), takes(Y,C), X \== Y),
+            L),
+    list_to_set(L,S),
+    member((X,Y),S).
+    
+teach_same_course(X,Y) :-
+    findall((X,Y),
+            (teaches(X,C), teaches(Y,C), X \== Y),
+            L),
+    list_to_set(L,S),
+    member((X,Y),S).            
+  
+teaches_multiples_courses(X) :-
+    findall(X,
+        (teaches(X,C), teaches(X,B), C \== B),
+        L),
+    list_to_set(L,S),
+    member(X,S). 
+    
+teaches_multiples_courses(X,S) :-
+    findall(X,
+        (teaches(X,C), teaches(X,B), C \== B),
+        L),
+    list_to_set(L,S).                                       
